7265b693316175e99eade2080b26ee777fbafe85
bryan sun <hitsjt@gmail.com>
2022-12-12 16:11:59 +0800

    I

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,5 +10,5 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <a href="https://www.wandoujia.com/apps/7461948/history_v190301" target="_blank">抖音版本下载</a>
     <br><br><br><br>
-    <a>助手下载</a>
+    <a target="_blank" href="/douyinv1.apk">助手下载</a>
 </div>
