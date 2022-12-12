c880f668584963a30f3c0b7390ae6819b965aec8
bryan sun <hitsjt@gmail.com>
2022-12-12 19:03:01 +0800

    I

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -19,14 +19,8 @@ use yii\console\Controller;
 class TestController extends Controller
 {
     public function actionIndex(){
-//        $ary = Yii::$app->services->jd->getInfoViaZTK('10040713168242');
-//        $ary = Yii::$app->services->jd->getItemInfo('67938515689',true);
-//        $ary = Yii::$app->services->jd->saveAfterGetItemInfo('67938515689');
-//        var_dump($ary);
 
-        $ary = file_get_contents("http://api-gw.haojingke.com/index.php/v1/api/jd/goodsdetail?apikey=1f72908997f3c187&goods_id=10040713168242");
-        echo "\n" .($ary) . "\n";
-//        echo date('y-m-d',1668355200);
+        Yii::$app->services->autojs->makeJiHuoma(10,time()+24*3600*60);
 
     }
 }
