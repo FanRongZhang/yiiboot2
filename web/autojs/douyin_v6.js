75d4fb5b7b29e7de868b1e879006c3d263575ff5
bryan sun <hitsjt@gmail.com>
2022-12-13 08:37:59 +0800

    I

--- web/autojs/douyin_v6.js
+++ web/autojs/douyin_v6.js
@@ -197,10 +197,10 @@ function tuijianzhaoqun(){
   var w = device.width
   var h = device.height
   var x = 660
-  var y = 640
+  var y = 580
   if( w == 720 && h == 1600){
     x = 660
-    y = 640
+    y = 580
   }else{
     x = w * x / w
     y = y * y / h
