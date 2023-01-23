a6fb749f0927832f388f6acc4249b09f92692a31
bryan sun <hitsjt@gmail.com>
2023-01-23 20:38:03 +0800

    jd

--- addons/autocellphone/Install.php
+++ addons/autocellphone/Install.php
@@ -11,7 +11,8 @@ use common\interfaces\AddonWidget;
  * 安装
  *
  * Class Install
- * @package addons\autocellphone */
+ * @package addons\autocellphone
+ */
 class Install extends Migration implements AddonWidget
 {
     /**
@@ -23,8 +24,8 @@ class Install extends Migration implements AddonWidget
     */
     public function run($addon)
     {
-        // MigrateHelper::upByPath([
-        //     '@addons/autocellphone/console/migrations/'
-        // ]);
+         MigrateHelper::upByPath([
+             '@addons/autocellphone/console/migrations/'
+         ]);
     }
 }
